732845af81ce5c8f75851a7bfda416a6be40980a
globalcanlitv <53581316+globalcanlitv@users.noreply.github.com>
2020-04-08 20:57:53 +0200

    Xxx

--- startapi.php
+++ startapi.php
@@ -1,7 +1,8 @@
-{"DeviceNo":"569982","DeviceHash":"fb2ec208d57c5b3dc540085cc7ac3fc447e16f47","updateStatus":false,"updateMessage":"
+{"DeviceNo":"569982","DeviceHash":"fb2ec208d57c5b3dc540085cc7ac3fc447e16f47","updateStatus":true,"updateMessage":"
 
 
 
 
 
-Uygulamay\u0131 kullanabilmek i\u00e7in g\u00fcncelleme yapman\u0131z gerekmektedir.","updateServer":"https://iptvpro93.github.io/download.html","AdsCount":4,"AdMobPublisherId":"pub-6886838813015191","AdMobBannerAds":true,"AdMobBannerAdUnitId":"ca-app-pub-6886838813015191/7288456481","AdMobInterstitialAds":true,"AdMobInterstitialAdUnitId":"ca-app-pub-6886838813015191/2192042470","AdMobInterstitialAggressiveMod":false,"MediaServerUrl":"https://github.com/globalcanlitv/file/raw/master/tvlist4.txt?Id=4&DeviceNo=569982&Key=d25d9e1d6bafda085c27d0366f028d2f63f58232","ReportServerUrl":""}
+Uygulamay\u0131 kullanabilmek i\u00e7in g\u00fcncelleme yapman\u0131z gerekmektedir.
+NOT:Yeni sürümü indirdiğinizde önce eskisini kaldırın sonra yenisini kurun.","updateServer":"https://iptvpro93.github.io/download.html","AdsCount":4,"AdMobPublisherId":"pub-6886838813015191","AdMobBannerAds":true,"AdMobBannerAdUnitId":"ca-app-pub-6886838813015191/7288456481","AdMobInterstitialAds":true,"AdMobInterstitialAdUnitId":"ca-app-pub-6886838813015191/2192042470","AdMobInterstitialAggressiveMod":false,"MediaServerUrl":"https://github.com/globalcanlitv/file/raw/master/tvlist4.txt?Id=4&DeviceNo=569982&Key=d25d9e1d6bafda085c27d0366f028d2f63f58232","ReportServerUrl":""}

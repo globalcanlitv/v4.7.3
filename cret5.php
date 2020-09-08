c6ef681011f0463691121b780caa7aac952b6a0b
globalcanlitv <53581316+globalcanlitv@users.noreply.github.com>
2020-09-08 20:51:13 +0200

    fff

--- cret5.php
+++ cret5.php
@@ -4,4 +4,4 @@
 
 
 
-Uygulamay\u0131 kullanabilmek i\u00e7in g\u00fcncelleme yapman\u0131z gerekmektedir.","updateServer":"https://iptvpro93.github.io/download.html","AdsCount":4,"AdMobPublisherId":"pub-5376148534134013","AdMobBannerAds":true,"AdMobBannerAdUnitId":"ca-app-pub-5376148534134013/6361416458","AdMobInterstitialAds":false,"AdMobInterstitialAdUnitId":"ca-app-pub-3940256099942544/1033173712","MediaServerUrl":"https://github.com/globalcanlitv/file/raw/master/tvlist4.txt?Id=2&DeviceNo=1167007&Key=0cb7200cfc19b246443a8404b4595e0dffa1b332","ReportServerUrl":""}
+Uygulamay\u0131 kullanabilmek i\u00e7in g\u00fcncelleme yapman\u0131z gerekmektedir.","updateServer":"https://globaltvpro.github.io/update/app.html","AdsCount":4,"AdMobPublisherId":"pub-5376148534134013","AdMobBannerAds":true,"AdMobBannerAdUnitId":"ca-app-pub-5376148534134013/6361416458","AdMobInterstitialAds":false,"AdMobInterstitialAdUnitId":"ca-app-pub-3940256099942544/1033173712","MediaServerUrl":"https://github.com/globalcanlitv/file/raw/master/tvlist4.txt?Id=2&DeviceNo=1167007&Key=0cb7200cfc19b246443a8404b4595e0dffa1b332","ReportServerUrl":""}
